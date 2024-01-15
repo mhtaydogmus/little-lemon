aa7a8383dfa1787147ee57e0a264c1416a76cbd9
Mehmet Aydoğmuş <93080871+mhtaydogmus@users.noreply.github.com>
2024-01-15 12:49:39 +0300

    Add files via upload

---
+++ LittleLemon-main/littlelemon/restaurant/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('menu/', MenuItemView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+]
